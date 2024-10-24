ed520ab46312405f6d09632c63889f261c8c99de
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2024-10-24 13:53:49 -0300

    Update pedidos.sql

--- pedidos.sql
+++ pedidos.sql
@@ -1,6 +1,6 @@
 create table pedidos(
   id serial primary key,
-  data_pedido data not null,
+  data_pedido date not null,
   valor numeric not null,
   id_clientes int,
   foreign key (id_clientes) references clientes(id)
